f5b909b9ef32586db60c791decde17b016f6bc73
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-10-25 14:15:44 -0300

    Add files via upload

---
+++ Soma0(1).java
@@ -0,0 +1,10 @@
+public class Soma0 {
+    public static void main(String[] args) {
+        int num1 = 48;
+        int num2 = 2; 
+
+        int resultado = num1 + num2;
+
+        System.out.println("A soma de "+ num1 +" e "+ num2 +" é igual a "+ resultado);
+    }
+}
